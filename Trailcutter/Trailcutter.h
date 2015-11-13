1351349c423c6566f295c524d2e77f74d95e9d4f
Tal Atlas <me@tal.by>
2015-11-12 22:08:32 -0500

    Initial Commit

---
+++ Trailcutter/Trailcutter.h
@@ -0,0 +1,19 @@
+//
+//  Trailcutter.h
+//  Trailcutter
+//
+//  Created by Tal Atlas on 11/12/15.
+//  Copyright © 2015 Range. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Trailcutter.
+FOUNDATION_EXPORT double TrailcutterVersionNumber;
+
+//! Project version string for Trailcutter.
+FOUNDATION_EXPORT const unsigned char TrailcutterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trailcutter/PublicHeader.h>
+
+
